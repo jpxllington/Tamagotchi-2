4f78ef03dce8f0e00094f369eb58e04fb6c68ab7
jpxllington <jjdp99@gmail.com>
2021-09-27 18:00:32 +0100

    dynamic variables for each animal added through push request

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { get } = require('http');
+const { join } = require('path/posix');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+var count = 1;
 
 class Tamagotchi{
-    constructor(name, type=dog, happiness =5 , hunger = 5){
+    constructor(name, type=dog, happiness =5 , hunger = 5, dead=false){
         this.name = name;
         this.type = type;
         this.happiness = happiness;
         this.hunger = hunger;
+        this.dead = dead
     }
     eat(){
         this.hunger = 0;
@@ -22,13 +26,15 @@ class Tamagotchi{
         this.hunger = Hunger;
         let happiness = parseInt(this.happiness) + 5;
         this.happiness = happiness;
+        isdead(this.name, Hunger, happiness);
     }
 }
 
-let animal;
+
 
 app.get('/', (req,res) => {
-    res.json({message:"Hi"});
+    allTamagotchi = getTamagotchi();
+    console.log(allTamagotchi.json());
 })
 
 app.post('/new', (req,res) => {
@@ -36,9 +42,12 @@ app.post('/new', (req,res) => {
     console.log(req);
     let name = req.body.petName;
     let type = req.body.petType;
-    res.json({ message: "Done"});
     console.log(`name is ${name} and type is ${type}`)
-    animal = new Tamagotchi(name, type, 5, 5)
+    let str = "let animal_" + count +" = new Tamagotchi( " + "name, " + "type" + ", 5, 5);"
+    eval(str);
+    res.json = (getTamagotchi());
+    count ++;
+
 })
 
 
@@ -50,11 +59,33 @@ if (Tamagotchi.happiness === 0){
     console.log(`${Tamagotchi.name} has run away`);
 }
 
-let Steve = new Tamagotchi('Steve','5','5');
+let animal_0 = new Tamagotchi('Steve', "Dog",'5','5');
 
-console.log(Steve.hunger);
-Steve.play();
-console.log(Steve.happiness)
-Steve.play();
+function isdead(name, hunger, happiness){
+    if (hunger >=10){
+        this.dead = true;
+        console.log(`${name} has died`)
+    }
+}
+count=1;
+let animal_1 = new Tamagotchi('bob','moose','6','6',true);
+
+console.log(animal_0.hunger);
+animal_0.play();
+console.log(animal_0.happiness)
+animal_0.play();
+
+getTamagotchi()
+
+function getTamagotchi(){
+    let allTamagotchi = [];
+    for (let a=0; a <= count; a++){
+        let tamagotchi;
+        let str = "tamagotchi = animal_" + a + ";";
+        eval(str);
+        allTamagotchi.push(tamagotchi);
+    }
+    return allTamagotchi.json();
+}
 
 module.exports = app;
