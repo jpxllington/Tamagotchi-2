b1fd7ab5ba8b26c38659bc6c24df60e3d311704e
jpxllington <jjdp99@gmail.com>
2021-09-30 17:02:55 +0100

    add card for each animal with proper placement. Missing a functional racoon image

--- client/static/index.js
+++ client/static/index.js
@@ -23,33 +23,33 @@ async function getTamagotchi(){
     console.log(tamagotchis.animal.length)
     for (let a = 0; a < tamagotchis.animal.length; a++){
         console.log(tamagotchis.animal[a]['name'])
-        animalName = tamagotchis.animal[a]['name'];
-        animalType = tamagotchis.animal[a]['type'];
-        animalHunger = tamagotchis.animal[a]['hunger'];
-        animalHappiness = tamagotchis.animal[a]['happiness'];
-        animalDead = tamagotchis.animal[a]['dead']; 
-        buildAnimal(animalName, animalType, animalHunger, animalHappiness, animalDead)
+        let animalName = tamagotchis.animal[a]['name'];
+        let animalType = tamagotchis.animal[a]['type'];
+        let animalHunger = tamagotchis.animal[a]['hunger'];
+        let animalHappiness = tamagotchis.animal[a]['happiness'];
+        let animalDead = tamagotchis.animal[a]['dead']; 
+        buildAnimal(animalName, animalType, animalHunger, animalHappiness, animalDead, a)
     }
 
 }
 
 getTamagotchi();
 
-function buildAnimal(name, type, hunger, happiness, dead){
+function buildAnimal(name, type, hunger, happiness, dead, a=0){
     let image;
     
     switch(type){
         case "cat":
-            image = "http://clipart-library.com/images/6ip5jXMMT.png";
+            image = "https://i.ebayimg.com/images/g/7WUAAOSwvzRX1Igm/s-l300.jpg";
             break
         case "dog":
             image = "https://cdn.pixabay.com/photo/2020/02/12/05/13/dog-cartoon-4841702_1280.jpg";
             break
         case "moose":
-            image = "https://www.iconspng.com/images/cartoon-moose/cartoon-moose.jpg";
+            image = "https://www.how-to-draw-funny-cartoons.com/images/cartoon-moose-9.gif";
             break
         case "racoon":
-            image = "https://www.nicepng.com/png/full/12-121463_cartoon-raccoon-png-jpg-freeuse-library-raccoon-cartoon.png";
+            image = "https://png.pngtree.com/png-vector/20200423/ourlarge/pngtree-cute-raccoon-cartoon-illustration-png-image_2191774.jpg";
             break
         case "slug":
             image ="https://ih1.redbubble.net/image.1164411977.9745/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg";
@@ -68,28 +68,67 @@ function buildAnimal(name, type, hunger, happiness, dead){
     let feed = document.createElement('button');
     let play = document.createElement('button');
     let kill = document.createElement('button');
+    let buttons = document.createElement('div');
+    parentDiv.setAttribute('id',`parentDiv${a}`)
+    parentDiv.setAttribute('class','parentDiv');
+    img.setAttribute('id',`photo${a}`);
+    img.setAttribute('class','photo');
+    nameplace.setAttribute('id',`animalname${a}`,);
+    nameplace.setAttribute('class','animalname');
+    stathunger.setAttribute('id',`hunger${a}`);
+    stathunger.setAttribute('class','hunger');
+    stats.setAttribute('class','stats')
+    stathappiness.setAttribute('id',`happiness${a}`);
+    stathappiness.setAttribute('class','happiness');
+    feed.setAttribute('id',`feed${a}`);
+    feed.setAttribute('class','button');
+    play.setAttribute('id',`play${a}`);
+    play.setAttribute('class','button');
+    kill.setAttribute('id',`kill${a}`);
+    kill.setAttribute('class','button');
+    buttons.setAttribute('class','buttons')
 
     nameplace.innerText = name;
     stathunger.innerText = `Hunger: ${hunger}`;
     stathappiness.innerText = `Happiness: ${happiness}`;
     img.src = image;
 
+    play.textContent = "Play";
+    feed.textContent = 'Feed';
+    kill.textContent = "Kill";
+
     stats.append(stathappiness, stathunger);
-    parentDiv.append(img, nameplace, stats, feed, play, kill);
+    buttons.append(feed,play,kill)
+    parentDiv.append(img, nameplace, stats, buttons);
 
     document.querySelector("#tamagotchiHolder").append(parentDiv);
 }
 
-function newAnimal(petName, petType){
+async function newAnimal(petName, petType){
     let data = {petName: petName,petType: petType}
     // console.log(JSON.stringify(data));
-    fetch('http://localhost:5000/new', {
-        method:"POST",
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json'}
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch((error) => { console.error('Error:',error)})
+
+    try {
+        let response = await fetch('http://localhost:5000/new', {
+                    method:"POST",
+                    body: JSON.stringify(data),
+                    headers: {'Content-Type': 'application/json'}
+                });
+        let tamagotchis = await response.json();
+        console.log(tamagotchis)
+        console.log(tamagotchis['name'])
+        let animalName = tamagotchis['name'];
+        let animalType = tamagotchis['type'];
+        let animalHunger = tamagotchis['hunger'];
+        let animalHappiness = tamagotchis['happiness'];
+        let animalDead = tamagotchis['dead']; 
+        buildAnimal(animalName, animalType, animalHunger, animalHappiness, animalDead)
+    } catch(err) { console.log(err)}
+
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // .catch((error) => { console.error('Error:',error)})
 }
+
+document.querySelector('.button').addEventListener('mouseover', () => {document.querySelector('.button').style.backgroundColor = "lightgray"});
 
